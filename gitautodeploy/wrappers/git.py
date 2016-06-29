bae9904714f0496474b6d4d1873e219297e7cd07
Andrew <andrei.osenenko@gmail.com>
2016-06-29 12:07:25 +0300

    fix

--- gitautodeploy/wrappers/git.py
+++ gitautodeploy/wrappers/git.py
@@ -31,6 +31,7 @@ class GitWrapper():
             commands.append('unset GIT_DIR')
 
         commands.append('git fetch ' + repo_config['remote'])
+        commands.append('git checkout ' + repo_config['branch'])
         commands.append('git reset --hard ' + repo_config['remote'] + '/' + repo_config['branch'])
         commands.append('git submodule update --init --recursive')
         #commands.append('git update-index --refresh')
