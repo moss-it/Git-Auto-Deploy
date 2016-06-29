6bc0f727fd0fb298c71b77cf48fb519a33a2dff6
Andrew <andrei.osenenko@gmail.com>
2016-06-29 13:08:05 +0300

    fix

--- deploy.py
+++ deploy.py
@@ -92,6 +92,9 @@ class Deploy(object):
         p = subprocess.Popen(["sudo", "pkgcache", "install", "bower"])
         p.wait()
 
+        p = subprocess.Popen(["sudo", "pkgcache", "bower", "install"])
+        p.wait()
+
     def update_json_config(self, app_config, global_settings):
 
         def update_config(config, route, value):
